3176fe979a2516bd8036521d43b1e8197f058ac2
Vitor Reis <166855207+ReisVC@users.noreply.github.com>
2024-07-16 10:05:39 -0300

    Delete exercicio directory

--- exercicio/MetodoDePagamento.ts
+++
@@ -1,4 +0,0 @@
-export interface MetodoPagamento {
-    nome:string
-    pagamento():void
-}
--- exercicio/Produto.ts
+++
@@ -1,8 +0,0 @@
-export interface Produto {
-    nome:string
-    peso:number
-    preco:number
-    tipo:string
-    comprar():void
-    valorTotal():number
-    }
